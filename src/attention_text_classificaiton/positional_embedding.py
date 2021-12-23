679286dbbb92b513affddaf4aba80dc4c5cb2dd0
Stefan <stefanmicic8697@gmail.com>
2021-12-23 13:07:13 +0100

    added english classifier with transformer and rnn

---
+++ src/attention_text_classificaiton/positional_embedding.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class TokenAndPositionEmbedding(layers.Layer):
+    def __init__(self, max_len, vocab_size, embed_dim):
+        super(TokenAndPositionEmbedding, self).__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=max_len, output_dim=embed_dim)
+
+    def call(self, x):
+        max_len = tf.shape(x)[-1]
+        positions = tf.range(start=0, limit=max_len, delta=1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
---
+++ src/attention_text_classificaiton/transformer_block.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim), ]
+        )
+        self.layer_normalization1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layer_normalization2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layer_normalization1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layer_normalization2(out1 + ffn_output)
